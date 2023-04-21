6111e5a25be22e97e8cdb9728a17432c0a42bd36
Shantanu Saha <sansaha0505@gmail.com>
2023-04-21 20:52:01 +0530

    first commit

--- src/components/About.js
+++ src/components/About.js
@@ -15,10 +15,12 @@ function About() {
                     </div>
                     <div className='col-md-6 aboutright'>
                         <h2 data-aos="fade-left" data-aos-duration="1000" >about me .</h2>
-                        <p data-aos="fade-left" data-aos-duration="1000" data-aos-offset="300" >Hi, I'm Shantanu Saha, a Freelance MERN Stack Developer with 1 plus year of experience in both front-end and back-end development, and performing quality assurance. Excellent knowledge in using HTML5, CSS3, SASS, JavaScript, ReactJS, Bootstrap, MongoDB, MYSQL, ExpressJS,Node.JS, Figma, and Firebase. I care about writing clean code and genuinely love developing new applications and learning more.
-                        </p>
-                        <p data-aos="fade-left" data-aos-duration="1000"  data-aos-offset="300">I enjoy taking a complex project and turning it into simple yet beautiful websites with great user experience.</p>
+                        <p data-aos="fade-left" data-aos-duration="1000" data-aos-offset="300" >Hello there! My name is Shantanu Saha, and I am a Freelance MERN Stack Developer with over a year of experience in the field. My expertise lies in both front-end and back-end development, as well as quality assurance.
+
+</p>
+                        <p data-aos="fade-left" data-aos-duration="1000"  data-aos-offset="300">With a deep understanding of HTML5, CSS3, SASS, JavaScript, ReactJS, Bootstrap, MongoDB, MYSQL, ExpressJS, Node.JS, Figma, and Firebase, I take great pride in developing clean and elegant code that not only functions flawlessly but also provides a remarkable user experience.</p>
                         {/* <p>I believe everything is an Art when you put your consciousness in it. You can connect with me via social links.</p> */}
+                        <p data-aos="fade-left" data-aos-duration="1000"  data-aos-offset="300">For me, taking a complex project and transforming it into a simple yet beautiful website that offers an exceptional user experience is what motivates me every day. I am passionate about learning new skills and staying up-to-date with the latest trends in the industry to ensure that I am always providing my clients with the best possible solutions.</p>
 
 
                     </div>
